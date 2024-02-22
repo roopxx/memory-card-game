393c7e07db4046fdbc3e989a329b958c3f735583
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-22 12:55:24 +0530

    Add StartPage, Header, Gallery components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
-import { LevelSelector } from "./components/LevelSelector";
+import { StartPage } from "./components/StartPage";
 import { Gallery } from "./components/Gallery";
 
 function App() {
+  const [start, setStart] = useState(true);
   const [difficulty, setDifficulty] = useState(8);
 
+  function startGame() {
+    setStart(!start);
+  }
   return (
-    <div className="container mx-auto">
-      <Header />
-      <LevelSelector onChoice={setDifficulty} />
-      <Gallery difficulty={difficulty} />
-    </div>
+    <>
+      {start ? (
+        <>
+          <StartPage setDifficulty={setDifficulty} onClick={startGame} />
+        </>
+      ) : (
+        <div className="container mx-auto">
+          <Header />
+          <Gallery difficulty={difficulty} />
+        </div>
+      )}
+    </>
   );
 }
 
