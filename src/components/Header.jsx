4a7908371168d2ee50e7f35fc42e26a76bd336ee
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-22 10:09:58 +0530

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <div className="py-4 text-center">
+        <h1 className="text-8xl font-['Creepster'] uppercase">
+          Memory card game
+        </h1>
+      </div>
+    </header>
+  );
+}
