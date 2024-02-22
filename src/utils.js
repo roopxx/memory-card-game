22edd23e1a25643f2a043ae9302411454c9a2ed9
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-23 00:48:43 +0530

    fix: filter out improper cards from API

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,17 @@ export function getCards(difficultyLevel) {
   return fetch(`https://rickandmortyapi.com/api/character/?page=${randomPage}`)
     .then((res) => res.json())
     .then((characters) => characters.results)
-    .then((cards) => cards.slice(0, difficultyLevel));
+    .then((cards) => {
+      // Filtering out improper card from fetch request due to either bad image or wrong card name
+      const filteredCards = cards.filter(
+        (card) =>
+          ![
+            6, 19, 20, 56, 61, 66, 95, 104, 130, 157, 189, 214, 222, 249, 269,
+            294, 297, 310, 315, 348, 349, 370, 399,
+          ].includes(card.id)
+      );
+      return shuffle([...filteredCards]).slice(0, difficultyLevel);
+    });
 }
 
 // Function to shuffle card array
