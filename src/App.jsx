036d6545a3ab250de65982509cc47166504ddc97
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-22 10:29:18 +0530

    Mount components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { LevelSelector } from "./components/LevelSelector";
+import { Gallery } from "./components/Gallery";
+
 function App() {
-  return <></>;
+  const [difficulty, setDifficulty] = useState(8);
+
+  return (
+    <div className="container mx-auto">
+      <Header />
+      <LevelSelector onChoice={setDifficulty} />
+      <Gallery difficulty={difficulty} />
+    </div>
+  );
 }
 
 export default App;
