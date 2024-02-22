c945c534acb4f07224d859788af6b6201adacac1
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-22 21:20:30 +0530

    Implement game logic and display score with result

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,9 +3,13 @@ import { getCards, shuffle } from "../utils";
 import { PropTypes } from "prop-types";
 import { useEffect } from "react";
 import { useState } from "react";
+import { ScoreBoard } from "./ScoreBoard";
+import { Result } from "./Result";
 
 export function Gallery({ difficulty }) {
   const [cardsArr, setCardsArr] = useState([]);
+  const [clickedCards, setClickedCards] = useState(new Set());
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     getCards(difficulty).then((cards) => {
@@ -13,21 +17,52 @@ export function Gallery({ difficulty }) {
     });
   }, [difficulty]);
 
+  function checkClickedCards(photoID) {
+    if (clickedCards.has(photoID)) {
+      localStorage.setItem("bestScore", 0);
+      setClickedCards(new Set());
+      setOpenDialog(true);
+    } else {
+      const newClickedCards = clickedCards.add(photoID);
+      setClickedCards(newClickedCards);
+    }
+    if (difficulty === clickedCards.size) {
+      setOpenDialog(true);
+    }
+  }
+
+  function updateScore(newScore) {
+    const currentBestScore = parseInt(localStorage.getItem("bestScore"));
+    const newBestScore = Math.max(currentBestScore, newScore);
+    localStorage.setItem("bestScore", newBestScore);
+  }
+
   return (
-    <section className="grid grid-cols-4 gap-8 mb-16">
-      {cardsArr.map((card) => (
-        <Card
-          key={card.id}
-          imageSrc={card.image}
-          imageText={card.name}
-          onClick={(e) => {
-            e.preventDefault();
-            const shuffledCardsArr = shuffle([...cardsArr]);
-            setCardsArr(shuffledCardsArr);
-          }}
+    <>
+      <ScoreBoard successClicks={clickedCards.size} />
+      <section className="grid grid-cols-4 gap-8 mb-16">
+        {cardsArr.map((card) => (
+          <Card
+            key={card.id}
+            imageSrc={card.image}
+            imageText={card.name}
+            onClick={(e) => {
+              e.preventDefault();
+              console.log(card.name);
+              updateScore(clickedCards.size + 1);
+              checkClickedCards(card.id);
+              const shuffledCardsArr = shuffle([...cardsArr]);
+              setCardsArr(shuffledCardsArr);
+            }}
+          />
+        ))}
+
+        <Result
+          openDialog={openDialog}
+          gameOutcome={difficulty === clickedCards.size}
         />
-      ))}
-    </section>
+      </section>
+    </>
   );
 }
 
