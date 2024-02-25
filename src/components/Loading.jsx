6ebc50f47c0ec1bfcb9973a96c0b129c38bdeda4
roopxx <127219908+roopxx@users.noreply.github.com>
2024-02-25 12:48:28 +0530

    Add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+      <h1 className="font-['Creepster'] text-9xl">Loading...</h1>
+    </div>
+  );
+}
